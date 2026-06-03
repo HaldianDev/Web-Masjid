6114f48a4d0cc957efdfbfb238c2f45a7ec37b5f
HaldianDev <haldian23@gmail.com>
2026-06-03 22:23:12 +0700

    fix: add fallback APP_KEY for Vercel deployment

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:TGaVaXwhWgjn9akhE4SCSIjGG/cQqvICUQ+PgLA3Bs0='),
 
     'previous_keys' => [
         ...array_filter(
